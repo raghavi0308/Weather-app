47e316ea9f05af4362f92d51b6841b0860469098
raghavi0308 <raghavijk3@gmail.com>
2025-06-16 03:17:44 +0530

    Error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Weather-app/',  // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
